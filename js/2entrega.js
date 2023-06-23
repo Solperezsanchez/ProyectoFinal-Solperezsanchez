eaf34a25911f69d630c712e7036472dc8c9e0456
Sol Eugenia Perez Sanchez <Solperezsanchez03@gmail.com>
2023-06-23 09:19:21 -0300

    agregamos un menu

--- js/2entrega.js
+++ js/2entrega.js
@@ -7,3 +7,34 @@ function pedirNombre() {
     alert(`¡Bienvenidx ${nombre} a nuestra tienda!`);
   }
 //   Comenzamos con el menu para que puedan seleccionar lo que deseen
+function menu() {
+    let opcionMenu = parseInt(
+      prompt(`
+      1 -Seleccionar productos
+      2 -Ver tu carrito
+      3 -Eliminar productos del carrito
+      4 -Pagar
+      `)
+    );
+  
+    switch (opcionMenu) {
+      case 1:
+        seleccionarProductos();
+        break;
+      case 2:
+        verCarrito();
+        break;
+      case 3:
+        eliminarProductos();
+        break;
+      case 4:
+        pagar();
+        break;
+      default:
+        alert(
+          "¡UPSS eso no esta en nuestro menu! Por favor selecciona algo que si este."
+        );
+        menu();
+    }
+  }
+  
