8a1c21c2b7e0857ff07e54d79f3d254c831fd6be
Sol Eugenia Perez Sanchez <Solperezsanchez03@gmail.com>
2023-07-13 04:10:41 -0300

    DOM

--- js/2entrega.js
+++ js/2entrega.js
@@ -7,172 +7,64 @@ function saludarCliente(nombre) {
   alert(`¡Bienvenidx ${nombre} a nuestra tienda!`);
 }
 pedirNombre();
-//   Comenzamos con el menu para que puedan seleccionar lo que deseen
-function menu() {
-  let opcionMenu = parseInt(
-    prompt(`
-      1 -Seleccionar productos
-      2 -Ver tu carrito
-      3 -Eliminar productos del carrito
-      4 -Pagar
-      `)
-  );
 
-  switch (opcionMenu) {
-    case 1:
-      seleccionarProductos();
-      break;
-    case 2:
-      verCarrito();
-      break;
-    case 3:
-      eliminarProductos();
-      break;
-    case 4:
-      pagar();
-      break;
-    default:
-      alert(
-        "¡UPSS eso no esta en nuestro menu! Por favor selecciona algo que si este."
-      );
-      menu();
+//MIS PRODUCTOS
+class producto{
+  constructor(id, nombre, precio, descripcion, imagen){
+     this.id = id,
+     this.nombre = nombre,
+     this.precio = precio,
+     this.descripcion = descripcion,
+     this.imagen = imagen
+
+  }
+  mostrarInfoProducto(){
+     console.log(`El textil ${this.nombre} es un  ${this.descripcion} y su precio es ${this.precio}`)
   }
 }
-//   Funcion para que seleccionen el producto que deseen
-function seleccionarProductos() {
-  let productoIngresado =
-    prompt(`Selecciona el numero que corresponda a tu HomeSpray deseado o en su defecto escribi "ESC" para regresar al menu anterior.
-       1.  ${cocoVai.nombre}: ${cocoVai.descripcion} - $${cocoVai.precio}
-       2. ${amour.nombre}: ${amour.descripcion} - $${amour.precio}
-       3. ${floresBlancas.nombre}: ${floresBlancas.descripcion} - $${floresBlancas.precio}
-       4. ${bamboo.nombre}: ${bamboo.descripcion} - $${bamboo.precio}
-       5. ${bubblegum.nombre}: ${bubblegum.descripcion} - $${bubblegum.precio}`);
+//Instanciación de objetos: 
+const producto1 = new producto(1,"Spray textil Coco Vai",710,"Spray de 250ml. Notas de salida:Coco. Notas de corazon:Nota lactea, Durazno. Notas de fondo: Coco, Vainilla", "textilCocoVai.jpg")
 
-  productoIngresado = productoIngresado.toUpperCase();
+const producto2 = new producto(2,"Spray textil Amour",710,"Spray de 250ml. Notas de salida: Flores de anis acarameladas, Flores de cerezo. Notas de corazon: Jazmin, Sandalo, Gardenia. Notas de fondo: Amber gris, Incienso, Vainilla." , "textilAmour.jpg")
 
-  while (productoIngresado != "ESC") {
-    switch (productoIngresado) {
-      case "1":
-        carrito.push(cocoVai);
-        precio = precio + cocoVai.precio;
-        alert(cocoVai.nombre + " se agrego al carrito de forma exitosa!");
-        break;
+const producto3 = new producto(3,"Spray textil Flores blancas", 710,"Spray de 250ml. Notas de salida: Ciclamen, Gardenia, Flor de naranjo dulce. Notas de corazon: Jazmin, Orquidea, Ylang Ylang. Notas de fondo: Almizcle, Balsamico." , "textilFloresBlancas.jpg")
 
-      case "2":
-        carrito.push(amour);
-        precio = precio + amour.precio;
-        alert(amour.nombre + " se agrego al carrito de forma exitosa!");
-        break;
+const producto4 = new producto(4,"Spray textil Bamboo",710, "Spray de 250ml. Notas de salida:Notas verdes frutales, Anana, Frutilla. Notas de corazon: Jazmin, Muguet, Rosa, Anis. Notas de fondo: Almizcle, Sandalo, Bambu." , "textilBamboo.jpg")
 
-      case "3":
-        carrito.push(floresBlancas);
-        precio = precio + floresBlancas.precio;
-        alert(floresBlancas.nombre + " se agrego al carrito de forma exitosa!");
-        break;
+const producto5 = new producto(5,"Spray textil Bubblegum", 710, "Spray de 250ml. Notas de salida:Cerezas, Naranja, Anana. Notas de corazon:Frutilla. Notas de fondo: Vinilla, Canela." , "textilBubblegum.jpg")
 
-      case "4":
-        carrito.push(bamboo);
-        precio = precio + bamboo.precio;
-        alert(bamboo.nombre + " se agrego al carrito de forma exitosa!");
-        break;
+//CREAR UN ARRAY DE OBJETOS
+const estanteria = []
+estanteria.push(producto1, producto2, producto3, producto4, producto5)
 
-      case "5":
-        carrito.push(bubblegum);
-        precio = precio + bubblegum.precio;
-        alert(bubblegum.nombre + " se agrego al carrito de forma exitosa!");
-        break;
+//capturar div productos
+let productosDiv = document.getElementById("productos")
 
-      default:
-        alert(
-          "Esta opcion no existe, por favor ingresa lo que esta en el menu."
-        );
-        break;
-    }
-
-    productoIngresado =
-      prompt(`Selecciona el numero que corresponda a tu HomeSpray deseado o en su defecto escribi "ESC" para regresar al menu anterior.
-          1.  ${cocoVai.nombre}: ${cocoVai.descripcion} - $${cocoVai.precio}
-          2. ${amour.nombre}: ${amour.descripcion} - $${amour.precio}
-          3. ${floresBlancas.nombre}: ${floresBlancas.descripcion} - $${floresBlancas.precio}
-          4. ${bamboo.nombre}: ${bamboo.descripcion} - $${bamboo.precio}
-          5. ${bubblegum.nombre}: ${bubblegum.descripcion} - $${bubblegum.precio}`);
-    productoIngresado = productoIngresado.toUpperCase();
+//recorrer estanteria para imprimir TOOODOS los elementos de mi array
+function mostrarCatalogo(array){
+  for(let producto of array ){
+     let nuevoProductoDiv = document.createElement("div")
+     //agregar class
+     nuevoProductoDiv.className = "col-12 col-md-6 col-lg-4 my-2"
+     nuevoProductoDiv.innerHTML = `<div id="${producto.id}    class="col">
+     <div class="card h-100">
+       <img
+         src="../img/productos/${producto.imagen}"
+         class="card-img-top"
+         alt="${producto.imagen}"
+       />
+       <div class="card-body">
+         <h5 class="card-title">${producto.nombre}</h5>
+         <h6 class="card-2title">$ ${producto.precio}</h6>
+         <p class="card-text">
+          ${producto.descripcion}
+         </p>
+         <a href="#" class="btn btn-primary">Añadir al carrito</a>
+       </div>
+     </div>
+   </div>`
+     productosDiv.appendChild(nuevoProductoDiv)
   }
-  menu();
-}
-//   Funcion para un nuevo pedido
-function nuevoPedido() {
-  pedido = [];
-  for (const x of carrito) {
-    pedido.push(x.nombre);
-  }
-}
-//   funcion para ver el carrito
-function verCarrito() {
-  nuevoPedido();
-  alert(`${pedido}`);
-  menu();
-}
-//   funcion para eliminar producto
-function eliminarProductos() {
-  for (const x of carrito) {
-    alert(x.nombre + x.descripcion + x.precio);
-    let productoEliminar = prompt("Eliminar produto. Escribi SI o NO");
-    productoEliminar = productoEliminar.toUpperCase();
-    if (productoEliminar == "SI") {
-      let posicion = carrito.indexOf(x);
-      carrito.splice(posicion, 1);
-      precio = precio - x.precio;
-      alert(x.nombre + " Se elimino de forma exitosa!");
-    }
-  }
-  menu();
-}
-//   funcion para pagar
-function pagar() {
-  nuevoPedido();
-  alert(`Tu pedido ${pedido}. 
-      Se cobrara $${precio}
-      Gracias por comprar en nuestra tienda!`);
-}
-//   productos
-const cocoVai = {
-  nombre: "Spray Coco Vai",
-  precio: 710,
-  descripcion:
-    "Spray de 250ml. Notas de salida:Coco. Notas de corazon:Nota lactea, Durazno. Notas de fondo: Coco, Vainilla",
-};
 
-const amour = {
-  nombre: "Spray Amour",
-  precio: 710,
-  descripcion:
-    "Spray de 250ml. Notas de salida: Flores de anis acarameladas, Flores de cerezo. Notas de corazon: Jazmin, Sandalo, Gardenia. Notas de fondo: Amber gris, Incienso, Vainilla.",
-};
-
-const floresBlancas = {
-  nombre: "Spray Flores Blancas.",
-  precio: 710,
-  descripcion:
-    "Spray de 250ml. Notas de salida: Ciclamen, Gardenia, Flor de naranjo dulce. Notas de corazon: Jazmin, Orquidea, Ylang Ylang. Notas de fondo: Almizcle, Balsamico.",
-};
-
-const bamboo = {
-  nombre: "Spray Bamboo",
-  precio: 710,
-  descripcion:
-    "Spray de 250ml. Notas de salida:Notas verdes frutales, Anana, Frutilla. Notas de corazon: Jazmin, Muguet, Rosa, Anis. Notas de fondo: Almizcle, Sandalo, Bambu.",
-};
-
-const bubblegum = {
-  nombre: " Spray Bubblegum",
-  precio: 710,
-  descripcion:
-    "Spray de 250ml. Notas de salida:Cerezas, Naranja, Anana. Notas de corazon:Frutilla. Notas de fondo: Vinilla, Canela.",
-};
-//   Variables
-
-let carrito = [];
-let pedido = [];
-let precio = 0;
-menu();
+}
+mostrarCatalogo(estanteria)
